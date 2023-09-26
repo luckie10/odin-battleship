5963d47d76108f443c40e9985ca0cc32371daec4
Boban Kilvin <btrajcevski10@gmail.com>
2023-09-26 02:21:22 -0400

    Change Gameboard to generate the entire board with null

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,14 +1,26 @@
 import Ship from "./ship";
 
 const Gameboard = (size = [10, 10]) => {
-  const board = new Map();
   let activeShips = 0;
+
+  const generateBoard = ([x, y]) => {
+    const board = new Map();
+
+    for (let i = 0; i < y; i++) {
+      for (let j = 0; j < x; j++) {
+        board.set(`${i}${j}`, null);
+      }
+    }
+    return board;
+  };
+
+  const board = generateBoard(size);
 
   const placeShip = (coords) => {
     const ship = Ship(coords.length);
 
     coords.map((coord) => {
-      if (board.has(coord))
+      if (board.get(coord))
         throw new Error(`A ship already exists at the ${coord} location`);
 
       board.set(coord, ship);
@@ -24,7 +36,7 @@ const Gameboard = (size = [10, 10]) => {
   };
 
   const recieveAttack = (coord) => {
-    if (!board.has(coord)) {
+    if (!board.get(coord)) {
       board.set(coord, "miss");
       return "miss";
     }
