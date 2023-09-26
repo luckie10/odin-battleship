60d2e8973168b2c5f0a3186e0178b87dc8be86a0
Boban Kilvin <btrajcevski10@gmail.com>
2023-09-26 00:34:17 -0400

    Add placeAttack and test for confirmed hit

--- src/modules/player.js
+++ src/modules/player.js
@@ -4,25 +4,29 @@ const Player = (gameboard, name = "") => {
   };
 
   const getRandomCoord = () => {
-    const yAxisDictionary = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+    // const yAxisDictionary = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
     const x = getRandomRange(0, 9);
-    const y = yAxisDictionary[getRandomRange(0, 9)];
+    const y = getRandomRange(0, 9);
+    // const y = yAxisDictionary[getRandomRange(0, 9)];
 
     return `${y}${x}`;
   };
 
   const placeRandomAttack = (gameboard) => {
     let randomCoord;
-    do randomCoord = getRandomCoord;
+    do randomCoord = getRandomCoord();
     while (gameboard.board.has(randomCoord));
 
     return gameboard.recieveAttack(randomCoord);
   };
 
+  const placeAttack = (gameboard, coord) => gameboard.recieveAttack(coord);
+
   return {
     name,
     gameboard,
     placeRandomAttack,
+    placeAttack,
   };
 };
 
--- src/modules/player.test.js
+++ src/modules/player.test.js
@@ -1,8 +1,16 @@
 import Player from "./player";
 import Gameboard from "./gameboard";
 
-test("placing an attack on oppponent gameboard", () => {
+test("Place an attack that misses", () => {
   const attacker = Player(Gameboard(), "attacker");
   const defeneder = Player(Gameboard(), "defender");
   expect(attacker.placeRandomAttack(defeneder.gameboard)).toBe("miss");
+});
+
+test("Place an attack that hits", () => {
+  const attacker = Player(Gameboard(), "attacker");
+  const board = Gameboard();
+  board.placeShip(["00", "01", "02"]);
+  const defeneder = Player(board, "defender");
+  expect(attacker.placeAttack(defeneder.gameboard, "01")).toBe("hit");
 });
