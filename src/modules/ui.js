66c3b3ee55eb09b0e3cbe5e88db2de8fd2a35180
Boban Kilvin <btrajcevski10@gmail.com>
2023-10-03 05:16:57 -0400

    Add UI to render the grids

---
+++ src/modules/ui.js
@@ -0,0 +1,71 @@
+import { createElement } from "../util";
+import Game from "./game";
+
+import "../style.scss";
+
+const UI = (() => {
+  const playerGridContainer = document.querySelector(".player-grid-container");
+  const opponentGridContainer = document.querySelector(
+    ".opponent-grid-container"
+  );
+
+  const updatePlayerGrid = (result, coord) => {
+    const cell = playerGridContainer.querySelector(`.grid .cell-${coord}`);
+    cell.classList.add(result);
+  };
+
+  const sendAttack = (opponent, key, target) => {
+    const result = opponent.recieveAttack(key);
+
+    if (result) target.classList.add(result);
+    Game.checkState();
+  };
+
+  const generateCell = (value, key, player, isPlayer) => {
+    const cell = createElement("div", { class: `grid-cell cell-${key}` });
+
+    if (value === "hit" || value === "miss") cell.classList.add(value);
+    else if (value) {
+      if (isPlayer) cell.classList.add("ship");
+      else
+        cell.addEventListener("click", () => sendAttack(player, key, cell), {
+          once: true,
+        });
+    } else {
+      if (!isPlayer)
+        cell.addEventListener("click", () => sendAttack(player, key, cell), {
+          once: true,
+        });
+    }
+
+    return cell;
+  };
+
+  const generateGrid = (player, isPlayer) => {
+    const { board, size } = player.gameboard;
+    const [x, y] = size;
+    const grid = createElement("div", { class: "grid" });
+    grid.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${y}, 1fr)`;
+
+    board.forEach((value, key) =>
+      grid.append(generateCell(value, key, player, isPlayer))
+    );
+
+    return grid;
+  };
+
+  const renderPlayerGrid = (player) =>
+    playerGridContainer.append(generateGrid(player, true));
+
+  const renderOpponentGrid = (player) =>
+    opponentGridContainer.append(generateGrid(player, false));
+
+  return {
+    updatePlayerGrid,
+    renderPlayerGrid,
+    renderOpponentGrid,
+  };
+})();
+
+export default UI;
